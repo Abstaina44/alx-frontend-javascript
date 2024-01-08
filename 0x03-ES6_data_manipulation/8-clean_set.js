2cb4d06f9b1e562283847f2cf28919599a333482
Abstaina44 <padcliff88@gmail.com>
2024-01-08 23:17:09 +0000

    A function named cleanSet that returns a string

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,19 @@
+export default function cleanSet(set, startString) {
+  const list = [];
+
+  if (
+    typeof set !== 'object'
+    || typeof startString !== 'string'
+    || startString.length === 0
+  ) {
+    return '';
+  }
+
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      list.push(item.slice(startString.length));
+    }
+  }
+
+  return list.join('-');
+}

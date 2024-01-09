54ebd5e55bfcae0ffb78d3a65546efdd572579d7
Abstaina44 <padcliff88@gmail.com>
2024-01-09 00:02:48 +0000

    Export a const instance of WeakMap and name it weakMap.

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,14 +1,27 @@
-export default function updateUniqueItems(map) {
-  const list = map;
+/**
+ * A weak map of endpoints and the number of calls made.
+ */
+export const weakMap = new WeakMap();
 
-  if (list instanceof Map) {
-    for (const [key, value] of list) {
-      if (value === 1) {
-        list.set(key, 100);
-      }
-    }
-  } else {
-    throw new Error('Cannot process');
+/**
+ * The maximum number of calls for an endpoint.
+ */
+const MAX_ENDPOINT_CALLS = 5;
+
+/**
+ * Tracks the number of calls made to an API's endpoint.
+ * @param {{
+ *   protocol: String,
+ *   name: String,
+ * }} endpoint - The endpoint to make a request to.
+ * @author Olatunde Oloyede <https://github.com/sirtuns4real>
+ */
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
   }
-  return list;
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
+    throw new Error('Endpoint load is high');
+  }
 }

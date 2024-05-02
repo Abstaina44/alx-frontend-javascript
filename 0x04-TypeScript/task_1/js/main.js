225d4bee51ec396be5287f5dd979f3116546b5b9
Ephraim Abstaina <48015890+Abstaina44@users.noreply.github.com>
2024-05-02 22:31:17 +0000

    Delete 0x04-TypeScript directory

--- 0x04-TypeScript/task_1/js/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.printTeacher = void 0;
-var printTeacher = function (firstName, lastName) {
-  return firstName[0] + ". " + lastName;
-};
-exports.printTeacher = printTeacher;
